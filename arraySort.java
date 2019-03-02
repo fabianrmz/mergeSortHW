68553728442c762d4b53d6cced80ed11507c9163
root <root@localhost.localdomain>
2019-03-01 19:57:17 -0600

    extra sorted with forkjoinpool

--- arraySort.java
+++ arraySort.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.ForkJoinPool;
-
-public class arraySort {
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+public class arraySort{
 	private int[] array;
 
 	public arraySort(int n){
@@ -10,7 +11,7 @@ public class arraySort {
 		}
 	}
 	
-	public int randomWithRange(int min, int max){
+	public static int randomWithRange(int min, int max){
 		   int range = (max - min) + 1;     
 		   return (int)(Math.random() * range) + min;
 	}
@@ -29,16 +30,19 @@ public class arraySort {
 	}
 	
 	private void mergeSorting(int left, int right) {
-		if(left<right){
-			int middle = (left+right)/2;
-			this.mergeSorting(left, middle);
-			this.mergeSorting(middle+1,right);
-			this.merge(left,middle,right);
-			
-		}
 		
+			if(left<right){
+				int middle = (left+right)/2;
+				this.mergeSorting(left, middle);
+				this.mergeSorting(middle+1,right);
+				this.merge(left,middle,right);
+				
+			}
+	
 		
 	}
+	
+	
 	
 	private void merge(int left, int middle, int right) {
 		int countLeft = middle - left + 1,
@@ -78,22 +82,115 @@ public class arraySort {
 	}
 	
 
+	public static class Parallel extends RecursiveAction {
+		
+		private int[] n;
+		private int left;
+		private int right;
+
+	    public Parallel(int[] array, int left, int right){
+	      this.n = array;
+	      this.left = left;
+	      this.right = right;
+	      
+	      
+	    }
+
+		@Override
+		protected void compute() {
+			if(left<right) {
+				int mid = (left+right)/2;
+				Parallel divideLeft = new Parallel(n, left, mid);
+				Parallel divideRight = new Parallel(n, mid+1, right);
+				
+				ForkJoinTask.invokeAll(divideLeft, divideRight);
+				this.merge(left, mid, right);
+				if(left==0 && right == this.n.length-1) {
+					//this.printArray(); uncomment this to see sorted list
+				}
+				
+			}
+			
+		}
+		
+		public void merge(int left, int middle, int right) {
+			int countLeft = middle - left + 1,
+				countRight = right - middle,
+				leftSide[] = new int [countLeft],
+	        	rightSide[] = new int [countRight]; 
+	        for (int i=0; i<countLeft; ++i) {
+	        	 leftSide[i] = this.n[left + i]; 
+	        }
+	        for (int j=0; j<countRight; ++j) {
+	        	rightSide[j] = this.n[middle + 1+ j]; 
+	        }
+	        int i = 0, 
+	        	j = 0,
+	        	k = left; 
+	        while (i < countLeft && j < countRight) { 
+	            if (leftSide[i] <= rightSide[j]) { 
+	                this.n[k] = leftSide[i]; 
+	                i++; 
+	            } 
+	            else{ 
+	                this.n[k] = rightSide[j]; 
+	                j++; 
+	            } 
+	            k++; 
+	        }
+	        while (i < countLeft) { 
+	            this.n[k] = leftSide[i]; 
+	            i++; 
+	            k++; 
+	        } 
+	        while (j < countRight) { 
+	            this.n[k] = rightSide[j]; 
+	            j++; 
+	            k++; 
+	        } 
+		}
+		
+		public void printArray() {
+			int array[] = this.n;
+			for(int l =0 ; l<array.length ; l++) {
+				System.out.println((l+1)+": "+array[l]+"\n");
+			}
+		}
+	    
+		
+	}
+
+
+	    
+	
 	public static void main(String[] args) {
 		int sizes[]= {4000,10000, 100000,1000000};
 		for(int i=0;i<sizes.length;i++) {
-			
 			arraySort a = new arraySort(sizes[i]);
 			System.out.println("Array size: "+a.array.length);
 			long startTime = System.nanoTime();
 			a.startMergeSort();
 			long endTime   = System.nanoTime();
 			long totalTime = endTime - startTime;
-			System.out.println("Time(seconds): "+(totalTime*(Math.pow(10, -9)))+"\n");	
-		}
-		
-			 
-		 
+			System.out.println("Time(seconds): "+(totalTime*(Math.pow(10, -9)))+" -> recursive way");	
+			///
+			
+			///
+			int arrayParallel[] = new int[4000];
+			for(int count=0;count<arrayParallel.length;count++) {
+				arrayParallel[count] = randomWithRange(0,99);
+			}
+			ForkJoinPool pool = new ForkJoinPool();
+			long sTime = System.nanoTime();
+			pool.invoke(new Parallel(arrayParallel, 0, arrayParallel.length-1));
+			long eTime   = System.nanoTime();
+			long tTime = eTime - sTime;
+			System.out.println("Time(seconds): "+(tTime*(Math.pow(10, -9)))+" -> ForkJoinPool \n --------------");	
+			
+		} 
 	}
+
+	
 
 }
 
